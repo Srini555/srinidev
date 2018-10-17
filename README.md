2a79d235906e737f9443f83246cf962b6d7d27db
srini55 <srinidupati@gmail.com>
2018-10-18 01:28:49 +0530

    comment

--- README.md
+++
@@ -1,2 +0,0 @@
-# srinidev
-dev
---
+++ demo-git/src/main/java/com/example/git/demogit/DemoGitApplication.java
@@ -0,0 +1,12 @@
+package com.example.git.demogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitApplication {
+//Srini
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitApplication.class, args);
+	}
+}
