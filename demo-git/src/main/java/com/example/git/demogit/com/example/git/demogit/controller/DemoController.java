e0fd5f996b648f1aff7ae213d52fcf74c591c38b
srini55 <srinidupati@gmail.com>
2018-10-23 23:45:31 +0530

    comment

--- demo-git/src/main/java/com/example/git/demogit/com/example/git/demogit/controller/DemoController.java
+++ demo-git/src/main/java/com/example/git/demogit/com/example/git/demogit/controller/DemoController.java
@@ -1,12 +1,11 @@
 package com.example.git.demogit.com.example.git.demogit.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-    // Srini
+
     @GetMapping(value = "/rest/getname/")
     public String geetName(){
 
