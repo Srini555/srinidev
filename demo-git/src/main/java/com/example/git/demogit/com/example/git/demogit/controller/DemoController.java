6154852967009fa029f7fb908abb12cd60c821fa
srini55 <srinidupati@gmail.com>
2018-10-24 00:06:43 +0530

    comment

--- demo-git/src/main/java/com/example/git/demogit/com/example/git/demogit/controller/DemoController.java
+++ demo-git/src/main/java/com/example/git/demogit/com/example/git/demogit/controller/DemoController.java
@@ -1,14 +1,20 @@
 package com.example.git.demogit.com.example.git.demogit.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.ApplicationScope;
 
 @RestController
 public class DemoController {
 
-    @GetMapping(value = "/rest/getname/")
+    @GetMapping(value = "/rest/getname/" )
     public String geetName(){
 
         return "Welcome Demo Controller";
+    }
+
+    public String postOperation( @RequestBody Employee employee){
+        return null;
     }
 }
