a85d31ecebac3e19f180786dc7f2cc46115cb437
srini55 <srinidupati@gmail.com>
2018-10-24 00:08:39 +0530

    comment

---
+++ demo-git/src/main/java/com/example/git/demogit/com/example/git/demogit/controller/model/Employee.java
@@ -0,0 +1,9 @@
+package com.example.git.demogit.com.example.git.demogit.controller.model;
+
+@Data
+public class Employee {
+    private String empName;
+    private String  eID;
+    private String eAddress;
+
+}
